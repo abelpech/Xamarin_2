5de093c95b911db6b8c29aea094ea6301a2a8f75
Abel Pech <abel.pech@gmail.com>
2020-04-30 23:02:55 -0700

    Add project files.

---
+++ PopupsTutorial/PopupsTutorial/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PopupsTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnDisplayAlertButtonClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "Esta es una ALERTA", "OK");
+        }
+
+        async void OnDisplayAlertQuestionButtonClicked(object sender, EventArgs e)
+        {
+            bool response = await DisplayAlert("Guardar?", "¿Quieres guardar tu informacion?", "Yes", "No");
+            Console.WriteLine("Save data: " + response);
+        }
+
+        async void OnDisplayActionSheetButtonClicked(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("Mandar a?", "Cancelar", null, "Email", "Twitter", "Facebook");
+            Console.WriteLine("Action: " + action);
+        }
+    }
+}
